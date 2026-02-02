dbcd87121acf40f05eaa740c0b5b351b3c40aa6c
Audrius <audrius@modernitvora.lt>
2026-02-02 13:57:39 +0200

    gallery done

--- app/components/Gallery/Main.vue
+++ app/components/Gallery/Main.vue
@@ -134,10 +134,10 @@ onUnmounted(() => {
         </button>
 
         <Transition
-          enter-active-class="transition-opacity duration-300"
+          enter-active-class="transition-opacity duration-100"
           enter-from-class="opacity-0"
           enter-to-class="opacity-100"
-          leave-active-class="transition-opacity duration-300"
+          leave-active-class="transition-opacity duration-100"
           leave-from-class="opacity-100"
           leave-to-class="opacity-0"
           mode="out-in"
